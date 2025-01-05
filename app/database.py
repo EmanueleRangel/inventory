5eea92373b375812b14f1a0938e149d35d978d46
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-05 08:26:36 -0300

    remov errors from post and get list

--- app/database.py
+++ app/database.py
@@ -1,11 +1,21 @@
 # app/database.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import DATABASE_URL  # Ajuste se necessário
 
-DATABASE_URL = "sqlite:///./test.db"  # Ajuste para o seu banco de dados, caso necessário.
+# Configuração do banco de dados
+SQLALCHEMY_DATABASE_URL = DATABASE_URL  # Exemplo: 'sqlite:///./test.db'
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  # Para SQLite
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+# Função get_db
+def get_db():
+    db = SessionLocal()  # Cria uma nova sessão
+    try:
+        yield db  # Retorna a sessão para ser usada nas rotas
+    finally:
+        db.close()  # Fecha a sessão após a execução
