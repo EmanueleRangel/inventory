0c17d0f0e4bd00653c31f2ef3b9ff67c914c7f22
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-05 12:55:33 -0300

    update models

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from app.database import Base  # Importando o Base de app.database
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    departamento = Column(String)
+    itens = Column(Integer)
+
 
 class ItemCreate(BaseModel):
     nome: str
@@ -8,3 +19,13 @@ class ItemCreate(BaseModel):
 
     class Config:
         orm_mode = True  # Permite que o Pydantic use o modelo SQLAlchemy
+
+class ItemResponse(BaseModel):
+    id: int
+    nome: str
+    departamento: str
+    itens: int
+
+    class Config:
+        orm_mode = True
+
