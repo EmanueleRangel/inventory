b7fa1034e246a10298e8fb796f1de561f543da4a
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-05 15:11:52 -0300

    clean cache and add tests

--- test_import.py
+++ test_import.py
@@ -1,4 +1,12 @@
-from app.models.models import Item
-from app.models.database import Base
+# test_import.py
 
-print("Imports funcionaram corretamente!")
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
+
+try:
+    from app.database import SessionLocal, engine, Base
+    print("Importação bem-sucedida!")
+except Exception as e:
+    print(f"Erro ao importar: {e}")
