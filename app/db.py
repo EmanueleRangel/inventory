10075c5ed89e35b9e9326084c5a0b1d6ede0402d
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-07 11:33:27 -0300

    add db.py

--- app/db.py
+++ app/db.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.models.database import get_db
+from app.models.models import Item
+
+# Obtenha uma instância da sessão
+db = next(get_db())
+
+# Agora você pode usar `db` para consultas
+result = db.query(Item).filter(Item.some_column == 'valor_especifico').all()
+
 
 DATABASE_URL = "sqlite:///./test.db"  # Substitua pela URL do seu banco de dados
 
